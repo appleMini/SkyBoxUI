ba3f1b87ea13bd9c0b6d5dbf98dd4a8d7374b5b7
appleMini <s1298web@163.com>
2018-03-07 10:56:52 +0800

    navigationBar 适配iphoneX

--- SkyBoxUI/Classes/Native/Main/View/SPNavigationBar.m
+++ SkyBoxUI/Classes/Native/Main/View/SPNavigationBar.m
@@ -16,13 +16,13 @@
     frame.size.height = 64;
     self.frame = frame;
     
-//    [self clear:self];
+    BOOL isIphoneX = [SPDeviceUtil isiPhoneX];
     Class navigationBarContentView = NSClassFromString(@"_UINavigationBarContentView");
     Class barBackground = NSClassFromString(@"_UIBarBackground");
     for (UIView *view in self.subviews) {
         if ([view isKindOfClass:navigationBarContentView] || [view isKindOfClass:barBackground]) {
             CGRect vframe = view.frame;
-            vframe.origin.y = (self.frame.size.height - vframe.size.height) / 2;
+            vframe.origin.y = isIphoneX ? 0 : (self.frame.size.height - vframe.size.height) / 2;
             view.frame = vframe;
         }
     }
